5bf1110739980839c8a49461cb7f98bbca819fa1
Rob Little <rlittletht@users.noreply.github.com>
2018-07-28 00:03:39 -0700

    hook up basic text listener

--- listest/Form1.Designer.cs
+++ listest/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.m_pbTestEvent = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // m_pbTestEvent
+            // 
+            this.m_pbTestEvent.Location = new System.Drawing.Point(713, 12);
+            this.m_pbTestEvent.Name = "m_pbTestEvent";
+            this.m_pbTestEvent.Size = new System.Drawing.Size(75, 23);
+            this.m_pbTestEvent.TabIndex = 0;
+            this.m_pbTestEvent.Text = "Test Event";
+            this.m_pbTestEvent.UseVisualStyleBackColor = true;
+            this.m_pbTestEvent.Click += new System.EventHandler(this.m_pbTestEvent_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.m_pbTestEvent);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button m_pbTestEvent;
     }
 }
 
--- listest/Form1.cs
+++ listest/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,21 @@ namespace listest
 {
     public partial class Form1 : Form
     {
+        private TraceSource m_ts;
+
         public Form1()
         {
             InitializeComponent();
+            m_ts = new TraceSource("listest");
+            for (int i = 0; i < Trace.Listeners.Count; i++)
+            {
+                m_ts.Listeners.Add(System.Diagnostics.Trace.Listeners[i]);
+            }
+        }
+
+        private void m_pbTestEvent_Click(object sender, EventArgs e)
+        {
+            m_ts.TraceEvent(TraceEventType.Critical, 1234, "{0}\t{1}\t{2}", Guid.NewGuid().ToString(), "Test", DateTime.Now.ToString());
         }
     }
 }
