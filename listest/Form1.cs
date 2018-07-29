67768bab2be378e7fd41d24c74d275fc33b53ae0
Rob Little <rlittletht@users.noreply.github.com>
2018-07-29 16:48:05 -0700

    added a a microtimer for tests

--- listest/Form1.cs
+++ listest/Form1.cs
@@ -30,7 +30,12 @@ namespace listest
 
         private void m_pbTestEvent_Click(object sender, EventArgs e)
         {
-            m_ts.TraceEvent(TraceEventType.Critical, 1234, "{0}\t{1}\t{2}", Guid.NewGuid().ToString(), "Test", DateTime.Now.ToString());
+            TimeThis("SingleEvent",
+                () =>
+                {
+                    m_ts.TraceEvent(TraceEventType.Critical, 1234, "{0}\t{1}\t{2}", Guid.NewGuid().ToString(), "Test",
+                        DateTime.Now.ToString());
+                });
         }
 
         protected override void OnClosing(CancelEventArgs e)
@@ -42,9 +47,15 @@ namespace listest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            m_ts.TraceEvent(TraceEventType.Critical, 1234, "{0}\t{1}\t{2}", Guid.NewGuid().ToString(), "Test Event 1", DateTime.Now.ToString());
-            m_ts.TraceEvent(TraceEventType.Critical, 1234, "{0}\t{1}\t{2}", Guid.NewGuid().ToString(), "Test Event 2", DateTime.Now.ToString());
-            m_ts.TraceEvent(TraceEventType.Critical, 1234, "{0}\t{1}\t{2}", Guid.NewGuid().ToString(), "Test Event 3", DateTime.Now.ToString());
+            TimeThis("ThreeEvents", () =>
+            {
+                m_ts.TraceEvent(TraceEventType.Critical, 1234, "{0}\t{1}\t{2}", Guid.NewGuid().ToString(),
+                    "Test Event 1", DateTime.Now.ToString());
+                m_ts.TraceEvent(TraceEventType.Critical, 1234, "{0}\t{1}\t{2}", Guid.NewGuid().ToString(),
+                    "Test Event 2", DateTime.Now.ToString());
+                m_ts.TraceEvent(TraceEventType.Critical, 1234, "{0}\t{1}\t{2}", Guid.NewGuid().ToString(),
+                    "Test Event 3", DateTime.Now.ToString());
+            });
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -55,6 +66,101 @@ namespace listest
         private void button3_Click(object sender, EventArgs e)
         {
             m_listener.TestResume();
+        }
+
+        delegate void GenericFun();
+
+        void TimeThis(string sAction, GenericFun fun)
+        {
+            MicroTimer timer = new MicroTimer();
+            timer.Start();
+            fun();
+            timer.Stop();
+
+            m_sb.AddMessage($"{sAction}: Elapsed {timer.ElapsedMsec()}");
+        }
+    }
+
+    // ============================================================================
+    // M I C R O  T I M E R
+    // ============================================================================
+    public class MicroTimer
+    {
+        private Stopwatch m_sw;
+
+        private int m_msec;
+        private int m_msecStop;
+
+        public MicroTimer()
+        {
+            m_sw = new Stopwatch();
+            m_sw.Start();
+
+            m_msec = Environment.TickCount;
+            m_msecStop = -1;
+        }
+
+        public void Reset()
+        {
+            m_sw.Reset();
+        }
+
+        public void Start()
+        {
+            m_sw.Start();
+        }
+
+        public void Stop()
+        {
+            m_sw.Stop();
+            m_msecStop = Environment.TickCount;
+        }
+
+        public double Seconds()
+        {
+            return m_sw.ElapsedMilliseconds / 1000.0;
+            // return (Environment.TickCount - m_msec) / 1000.0;
+        }
+
+        public long Microsec
+        {
+            get
+            {
+                return m_sw.ElapsedTicks / (Stopwatch.Frequency / (1000L * 1000L));
+            }
+        }
+
+        public long Msec()
+        {
+            return m_sw.ElapsedMilliseconds;
+            // return m_msecStop - m_msec;
+        }
+
+        public double MsecFloat
+        {
+            get
+            {
+                return ((double)Microsec) / 1000.0;
+            }
+        }
+        public string Elapsed()
+        {
+            if (m_sw.IsRunning)
+                m_sw.Stop();
+            //if (m_msecStop == -1)
+            //Stop();
+
+            return String.Format("{0}", Seconds().ToString());
+        }
+
+        public string ElapsedMsec()
+        {
+            if (m_sw.IsRunning)
+                m_sw.Stop();
+            //if (m_msecStop == -1)
+            //Stop();
+
+            return Msec().ToString();
         }
     }
 
