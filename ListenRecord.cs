75339644b5df0334acfcca3e72dd9f174b74a35b
Rob Little <rlittletht@users.noreply.github.com>
2018-08-04 21:00:20 -0700

    lots of comments and formatting

--- ListenRecord.cs
+++ ListenRecord.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 using System.Threading;
 using NUnit.Framework;
 using TCore.Pipeline;
@@ -25,10 +26,8 @@ namespace TCore.ListenAz
         private int m_nTid;
         private string m_sMessage;
 
-        public ListenRecord()
-        {
-
-        }
+        // empty constructure for NUnit
+        public ListenRecord() { }
 
         #region Constructors / Initialization
 
@@ -121,6 +120,8 @@ namespace TCore.ListenAz
 
         #endregion
 
+        #region Partitions
+
         public Partition Part => m_part;
 
         // ============================================================================
@@ -190,11 +191,20 @@ namespace TCore.ListenAz
             return part;
         }
 
+        /*----------------------------------------------------------------------------
+        	%%Function: PartToString
+        	%%Qualified: TCore.ListenAz.ListenRecord.PartToString
+        	%%Contact: rlittle
+        	
+            get a canonical string representation for the part
+        ----------------------------------------------------------------------------*/
         public static string PartToString(Partition part)
         {
             return $"{part.Year}/{part.Month}/{part.Day}/{part.Hour}";
         }
+        #endregion
 
+        #region Unit Tests
         [Test]
         [TestCase("4/11/1972 12:00 +07:00", "1972/04/11/05")]
         [TestCase("4/11/1972 0:00 +07:00", "1972/04/10/17")]
@@ -225,7 +235,9 @@ namespace TCore.ListenAz
             Partition partRight = Partition.Zero;
             Assert.AreEqual(partLeft, partRight);
         }
+        #endregion
 
+        #region Log/String Generation
         /*----------------------------------------------------------------------------
         	%%Function: EventTypeToString
         	%%Qualified: TCore.ListenAz.ListenRecord.EventTypeToString
@@ -254,6 +266,13 @@ namespace TCore.ListenAz
         public static string s_sCsvHeader =
             "date,eventTickCount,level,applicationName,eventID,instanceID,processID,threadID,message";
 
+        /*----------------------------------------------------------------------------
+        	%%Function: ToCsv
+        	%%Qualified: TCore.ListenAz.ListenRecord.ToCsv
+        	%%Contact: rlittle
+        	
+            Convert this ListenRecord into comma separated string 
+        ----------------------------------------------------------------------------*/
         public string ToCsv()
         {
             Partition part = PartitionParse(m_dttm);
@@ -263,6 +282,13 @@ namespace TCore.ListenAz
                 $"{sDate},{m_dwTickCount},{EventTypeToString(m_tetEventType)},\"{m_sAppName}\",{m_nEventID},{m_nInstanceID},{m_nPid},{m_nTid},\"{m_sMessage}\"";
         }
 
+        /*----------------------------------------------------------------------------
+        	%%Function: ToString
+        	%%Qualified: TCore.ListenAz.ListenRecord.ToString
+        	%%Contact: rlittle
+        	
+            Get a string representation for this ListenRecord
+        ----------------------------------------------------------------------------*/
         public override string ToString()
         {
             Partition part = PartitionParse(m_dttm);
@@ -270,7 +296,10 @@ namespace TCore.ListenAz
             return
                 $"{PartToString(part)}: {m_dttm.ToString()}, tick({m_dwTickCount}), {EventTypeToString(m_tetEventType)}, {m_sAppName}, eid({m_nEventID}), inst({m_nInstanceID}), pid({m_nPid}), tid({m_nTid}), {m_sMessage}";
         }
+        #endregion
     }
+
+    // GENERAL APPROACH TO THE 2 STAGE PIPELINE
 
     // this will setup stage 1 pipeline, ready for the Listener to give us records and we will get them
     // into persistent local storage.  we will use our session ID as a directory to store the logs into
@@ -293,7 +322,15 @@ namespace TCore.ListenAz
     //          is right after the last record processed)
     //      If DONE FLAG is set on record, then DELETE FILENAME and DELETE FILENAME.PROCESSED
 
-
+    // ============================================================================
+    // S T A G E   1
+    //
+    // This is the hot part of the pipeline. ListenRecords take care of recording
+    // the log information from the listener on the main thread, then on another 
+    // thread, these reorded log messages are written to durable storage (filesystem)
+    // and a ListenSync is created to the sync pipeline (which will take care of
+    // syncing durable storage to azure)
+    // ============================================================================
     public class Stage1
     {
         private ProducerConsumer<ListenRecord> m_pipeHot;
@@ -302,50 +339,91 @@ namespace TCore.ListenAz
         private listener.IHookListen m_ihl;
         private Stage2 m_stage2;
 
-        private string
-            m_sLogFolder; // this is something like "widgetFinderLogs" - initialized when the listener is created (and consistent across sessions)
+        // this is something like "widgetFinderLogs" - initialized when the listener is created (and consistent across sessions)
+        private string m_sLogFolder;
 
-        private int
-            m_nTestOffsetMinutesDateTime; // this allows a test harness to mess with the datetime stamp. this value will be added to the datetime.now() when we create the listenrecord (the value is treated as minutes)
+        // this allows a test harness to mess with the datetime stamp. this value will be added to the datetime.now() when we create the listenrecord (the value is treated as minutes)
+        private int m_nTestOffsetMinutesDateTime;
 
+        /*----------------------------------------------------------------------------
+        	%%Function: Stage1
+        	%%Qualified: TCore.ListenAz.Stage1.Stage1
+        	%%Contact: rlittle
+        	
+            Create the ListenRecorder (the hot part of the pipe).
+            This stage requires the other stage to already be created.
+        ----------------------------------------------------------------------------*/
         public Stage1(listener.IHookListen ihl, Stage2 stage2, string sLogFolder = "TestLogs")
         {
             m_stage2 = stage2;
-            m_pipeHot = new ProducerConsumer<ListenRecord>((string sMessage) => { ihl.WriteLine(sMessage); },
-                ProcessQueuedRecord);
+            m_pipeHot = new ProducerConsumer<ListenRecord>((string sMessage) => { ihl.WriteLine(sMessage); }, ProcessQueuedRecord);
 
             m_guidSession = Guid.NewGuid();
             m_sLogFolder = sLogFolder;
             m_sFileRoot = Path.Combine(Path.GetTempPath(), m_sLogFolder, $"{m_guidSession.ToString()}");
             Directory.CreateDirectory(m_sFileRoot);
 
-            m_sCurrentFile = null;
-            m_partCurrent = ListenRecord.Partition.Zero;
             m_ihl = ihl;
 
             m_pipeHot.Start();
         }
 
+        /*----------------------------------------------------------------------------
+        	%%Function: Stop
+        	%%Qualified: TCore.ListenAz.Stage1.Stop
+        	%%Contact: rlittle
+        	
+            Stop the hot pipe. This does not stop the sync pipe
+        ----------------------------------------------------------------------------*/
         public void Stop()
         {
             m_pipeHot.Stop();
         }
 
+        /*----------------------------------------------------------------------------
+        	%%Function: SetTestOffsetMinutesDateTime
+        	%%Qualified: TCore.ListenAz.Stage1.SetTestOffsetMinutesDateTime
+        	%%Contact: rlittle
+        	
+            set an internal offset for the recorder. this allows us to simulate
+            crossing partition boundaries
+        ----------------------------------------------------------------------------*/
         public void SetTestOffsetMinutesDateTime(int nMinutes)
         {
             m_nTestOffsetMinutesDateTime = nMinutes;
         }
 
+        /*----------------------------------------------------------------------------
+        	%%Function: TestSuspendConsumerThread
+        	%%Qualified: TCore.ListenAz.Stage1.TestSuspendConsumerThread
+        	%%Contact: rlittle
+        	
+            Suspend the consumer thread (used for debugging only)
+        ----------------------------------------------------------------------------*/
         public void TestSuspendConsumerThread()
         {
             m_pipeHot.TestSuspendConsumerThread();
         }
 
+        /*----------------------------------------------------------------------------
+        	%%Function: TestResumeConsumerThread
+        	%%Qualified: TCore.ListenAz.Stage1.TestResumeConsumerThread
+        	%%Contact: rlittle
+        	
+            resume the consumer thread
+        ----------------------------------------------------------------------------*/
         public void TestResumeConsumerThread()
         {
             m_pipeHot.TestResumeConsumerThread();
         }
 
+        /*----------------------------------------------------------------------------
+        	%%Function: RecordNewListenRecord
+        	%%Qualified: TCore.ListenAz.Stage1.RecordNewListenRecord
+        	%%Contact: rlittle
+        	
+            create a new ListenRecord for this message and post it to the hotPipe
+        ----------------------------------------------------------------------------*/
         public void RecordNewListenRecord(string sMessage)
         {
             ListenRecord lr;
@@ -362,17 +440,31 @@ namespace TCore.ListenAz
             m_pipeHot.Producer.QueueRecord(lr);
         }
 
-        // THESE VALUES CANNOT BE TOUCHED OUTSIDE OF THE CONSUMER THREAD (once the thread is started)
-        private string m_sCurrentFile;
-        private int m_cCurRecords;
-        private ListenRecord.Partition m_partCurrent;
-        private long m_ibOffsetLim;
-
         private static readonly int s_cMaxRecords = 1000;
 
+        #region ListenRecordFile
+
+        // ============================================================================
+        // L I S T E N  R E C O R D  F I L E
+        //
+        // The Source/Sink mechanism between the hot pipe and the sync pipe is a 
+        // persistent file on the disk.  The hot pipe gets WRITE access to the file
+        // and the sync pipe gets only READ access. We stricly append to the file,
+        // so the sync pipe can always get at the data that the hot pipe has posted
+        // to it.
+        //
+        // In order to do this, we need to be explicit about the seek positions that
+        // the sync pipe is supposed to look at, which means we have to take care
+        // of our own filestream writing (buffered streams will cause problems)
+        //
+        // This file also takes care of knowing how many records have been written, 
+        // as well as what partition this file represents (so we can determing if
+        // future ListenRecords belong to the same partition)
+        // ============================================================================
         public class ListenRecordFile
         {
-            private StreamWriter m_sw;
+            // private StreamWriter m_sw;
+            private FileStream m_fs;
             private string m_sCurrentFile;
             private int m_cCurRecords;
             private int m_cCurRecordsStart;
@@ -384,6 +476,14 @@ namespace TCore.ListenAz
             public ListenRecord.Partition Part => m_partCurrent;
             public long FileOffsetLim => m_ibOffsetLim;
 
+            /*----------------------------------------------------------------------------
+            	%%Function: ListenRecordFile
+            	%%Qualified: TCore.ListenAz.Stage1.ListenRecordFile.ListenRecordFile
+            	%%Contact: rlittle
+            	
+                Create a new ListenRecord file -- this is just a <GUID>.csv appended
+                to the FileRoot we are given
+            ----------------------------------------------------------------------------*/
             public ListenRecordFile(string sFileRoot, ListenRecord.Partition part)
             {
                 Guid guidFile = Guid.NewGuid();
@@ -394,30 +494,55 @@ namespace TCore.ListenAz
                 m_partCurrent = part;
             }
 
+            /*----------------------------------------------------------------------------
+            	%%Function: Flush
+            	%%Qualified: TCore.ListenAz.Stage1.ListenRecordFile.Flush
+            	%%Contact: rlittle
+            	
+                Flush the file and make note of the lim seek position (so we can 
+                inform the sync pipe)
+            ----------------------------------------------------------------------------*/
             public long Flush()
             {
-                if (m_sw == null)
+                if (m_fs == null)
                     return -1;
 
-                m_sw.Flush();
-                m_ibOffsetLim = m_sw.BaseStream.Position;
+                m_fs.Flush();
+                m_ibOffsetLim = m_fs.Position;
                 return m_ibOffsetLim;
             }
 
+            /*----------------------------------------------------------------------------
+            	%%Function: FlushAndClose
+            	%%Qualified: TCore.ListenAz.Stage1.ListenRecordFile.FlushAndClose
+            	%%Contact: rlittle
+            	
+                flush the underlying file and CLOSE it. this will let us tell the 
+                sync pipe that we are done with the file, and the sync pipe now
+                gets to take ownership of the file (and delete it if necessary)
+            ----------------------------------------------------------------------------*/
             public long FlushAndClose()
             {
-                if (m_sw == null)
+                if (m_fs == null)
                     return -1;
 
-                m_sw.Flush();
-                m_ibOffsetLim = m_sw.BaseStream.Position;
-                m_sw.Close();
-                m_sw.Dispose();
-                m_sw = null;
+                m_fs.Flush();
+                m_ibOffsetLim = m_fs.Position;
+                m_fs.Close();
+                m_fs.Dispose();
+                m_fs = null;
 
                 return m_ibOffsetLim;
             }
 
+            /*----------------------------------------------------------------------------
+            	%%Function: FCanAppendPartitionRecord
+            	%%Qualified: TCore.ListenAz.Stage1.ListenRecordFile.FCanAppendPartitionRecord
+            	%%Contact: rlittle
+            	
+                Determine if the file is too big, or if the partitions don't match. if either
+                is true, return false (we cannot append to this file)
+            ----------------------------------------------------------------------------*/
             public bool FCanAppendPartitionRecord(ListenRecord.Partition part)
             {
                 if (m_cCurRecords > s_cMaxRecords || m_partCurrent != part)
@@ -426,21 +551,58 @@ namespace TCore.ListenAz
                 return true;
             }
 
+            static byte[] s_rgbNewline = { 0x0d, 0x0a };
+
+            /*----------------------------------------------------------------------------
+            	%%Function: WriteLine
+            	%%Qualified: TCore.ListenAz.Stage1.ListenRecordFile.WriteLine
+            	%%Contact: rlittle
+            	
+                Our version of WriteLine, uses UTF8
+            ----------------------------------------------------------------------------*/
+            void WriteLine(string sFile)
+            {
+                if (m_fs == null)
+                    m_fs = new FileStream(m_sCurrentFile, FileMode.Append, FileAccess.Write, FileShare.Read);
+
+                byte[] rgb;
+
+                rgb = Encoding.UTF8.GetBytes(sFile);
+
+                m_fs.Write(rgb, 0, rgb.Length);
+                m_fs.Write(s_rgbNewline, 0, 2);
+            }
+
+            /*----------------------------------------------------------------------------
+            	%%Function: WriteListenRecord
+            	%%Qualified: TCore.ListenAz.Stage1.ListenRecordFile.WriteListenRecord
+            	%%Contact: rlittle
+            	
+                Write the given ListenRecord to this file
+            ----------------------------------------------------------------------------*/
             public void WriteListenRecord(ListenRecord lr)
             {
-                if (m_sw == null)
-                    m_sw = new StreamWriter(m_sCurrentFile, true);
-
                 if (m_cCurRecords == 0)
-                    m_sw.WriteLine(ListenRecord.s_sCsvHeader);
+                    WriteLine(ListenRecord.s_sCsvHeader);
 
-                m_sw.WriteLine(lr.ToCsv());
+                WriteLine(lr.ToCsv());
+                
                 m_cCurRecords++;
             }
         }
+        #endregion
 
+        // THIS CANNOT BE TOUCHED OUTSIDE OF THE CONSUMER THREAD (once the thread is started)
         private ListenRecordFile m_lrfCurrent;
 
+        /*----------------------------------------------------------------------------
+        	%%Function: ProcessQueuedRecord
+        	%%Qualified: TCore.ListenAz.Stage1.ProcessQueuedRecord
+        	%%Contact: rlittle
+        	
+            Process a record that was queued for us. Save the records to persistent
+            storage, and let the sync pipe know about them.
+        ----------------------------------------------------------------------------*/
         public void ProcessQueuedRecord(IEnumerable<ListenRecord> pllr)
         {
             IEnumerator<ListenRecord> enumerator = pllr.GetEnumerator();
@@ -451,8 +613,7 @@ namespace TCore.ListenAz
                 return;
 
             ListenRecord lr = enumerator.Current;
-            int cCurRecordsStart = m_cCurRecords;
-
+            
             if (lr == null)
                 return;
 
@@ -486,10 +647,8 @@ namespace TCore.ListenAz
             }
 
             // LASTLY, send a notification for the current file and the current record
-            m_lrfCurrent.FlushAndClose();
+            //m_lrfCurrent.FlushAndClose();  // we don't stricly have to close the file since we told the other thread we are NOT hands off of the file
             m_stage2.RecordNewListenSync(m_lrfCurrent, false);
-
-            // if m_sCurrentFile != null, m_stage2.Producer2.Post(m_sCurrentFile, s_cMaxRecords, m_partCurrent, DONE==FALSE
         }
     }
 }
